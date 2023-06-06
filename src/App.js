e14fafe6362cf696c1cc8c9a38e9a2db6e2acd54
Kangin Lee <cjsworkddls1@naver.com>
2023-06-07 00:03:39 +0900

    update color sensor ui

--- src/App.js
+++ src/App.js
@@ -1,32 +1,96 @@
 import React, { useEffect, useState } from "react";
+import "./App.css"; // CSS 파일의 상대 경로
 import axios from "axios";
+import { Container } from "react-bootstrap";
+
+const sensorColor = {
+  Gray: {
+    name: "GRAY",
+    combination: ["GRAY", "NAVY", "BEIGE", "BLUE", "KHAKI"],
+  },
+  Black: {
+    name: "BLACK",
+    combination: ["BLACK", "WHITE", "KHAKI", "BLUE", "GREEN"],
+  },
+  White: {
+    name: "WHITE",
+    combination: ["BLACK", "BLUE", "BEIGE", "KHAKI", "GRAY"],
+  },
+  Navy: {
+    name: "NAVY",
+    combination: ["GRAY", "BLUE", "BEIGE", "CREAM", "KHAKI"],
+  },
+  Khaki: {
+    name: "KHAKI",
+    combination: ["GRAY", "BLUE", "BEIGE", "BLACK", "BROWN"],
+  },
+  Green: {
+    name: "GREEN",
+    combination: ["BEIGE", "BLUE", "WHITE", "BLACK", "GRAY"],
+  },
+  Blue: {
+    name: "BLUE",
+    combination: ["BEIGE", "WHITE", "GRAY", "BLACK", "KHAKI"],
+  },
+  Sky_Blue: {
+    name: "SKYBLUE",
+    combination: ["BEIGE", "WHITE", "GRAY", "BLACK", "KHAKI"],
+  },
+  Brown: {
+    name: "BROWN",
+    combination: ["BEIGE", "BLUE", "CREAM", "BLACK", "KHAKI"],
+  },
+  Beige: {
+    name: "BEIGE",
+    combination: ["BROWN", "NAVY", "CREAM", "BLACK", "BLUE"],
+  },
+  Orange: {
+    name: "ORANGE",
+    combination: ["BEIGE", "NAVY", "KHAKI", "BLACK", "CREAM"],
+  },
+  Red: {
+    name: "RED",
+    combination: ["GREEN", "ORANGE", "BLUE", "BLACK", "BEIGE"],
+  },
+  Purple: {
+    name: "PURPLE",
+    combination: ["SKYBLUE", "PURPLE", "WHITE", "BEIGE", "BLACK"],
+  },
+  Pink: {
+    name: "PINK",
+    combination: ["SKYBLUE", "BLACK", "KHAKI", "WHITE", "GRAY"],
+  },
+  Yellow: {
+    name: "YELLOW",
+    combination: ["BLUE", "CREAM", "BROWN", "WHITE", "BLACK"],
+  },
+  Burgundy: {
+    name: "BURGUNDY",
+    combination: ["BEIGE", "NAVY", "KHAKI", "BLACK", "GRAY"],
+  },
+};
 
 const App = () => {
-  const [backendData, setBackendData] = useState([{}]);
+  // const [backendData, setBackendData] = useState([{}]);
   const [dominantColors, setDominantColors] = useState([]);
 
   useEffect(() => {
-    fetchBackendData();
+    // fetchBackendData();
     fetchDominantColors();
   }, []);
 
-  // useEffect(() => {
-  //   fetch("http://localhost:5000/api")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setBackendData(data);
+  console.log("asd", sensorColor.name);
+
+  // const fetchBackendData = () => {
+  //   axios
+  //     .get("https://port-0-color-server-dcse2bligx5y07.sel4.cloudtype.app/api")
+  //     .then((response) => {
+  //       setBackendData(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log("Failed to fetch backend data:", error);
   //     });
-  // }, []);
-  const fetchBackendData = () => {
-    axios
-      .get("https://port-0-color-server-dcse2bligx5y07.sel4.cloudtype.app/api")
-      .then((response) => {
-        setBackendData(response.data);
-      })
-      .catch((error) => {
-        console.log("Failed to fetch backend data:", error);
-      });
-  };
+  // };
 
   const fetchDominantColors = () => {
     axios
@@ -40,27 +104,48 @@ const App = () => {
         console.log("Failed to fetch dominant colors:", error);
       });
   };
+  console.log("sad", sensorColor.Beige.combination);
 
   return (
-    <div>
-      {typeof backendData.users === "undefined" ? (
+    <div className="parent-container">
+      <Container>
+        {/* {typeof backendData.users === "undefined" ? (
         <p>Loding...</p>
       ) : (
         backendData.users.map((user, i) => <p key={i}>{user}</p>)
-      )}
+      )} */}
 
-      <div>
-        {dominantColors.length === 0 ? (
-          <p>Loading...</p>
-        ) : (
-          dominantColors.map((color, index) => (
-            <div>
-              <span>인식한 색상: </span>
-              <p key={index}>{color}</p>
-            </div>
-          ))
-        )}
-      </div>
+        <div>
+          {dominantColors.length === 0 ? (
+            <p>Loading...</p>
+          ) : (
+            dominantColors.map((color, index) => {
+              const colorInfo = sensorColor[color];
+              if (!colorInfo) return null;
+              return (
+                <div key={index}>
+                  <div className="sensor-color">인식한 색상</div>
+                  <div id="sensor-get-color" className={colorInfo.name}>
+                    {colorInfo.name}
+                  </div>
+                  <div className="combination-color">추천 색상</div>
+                  <div className="recommend-color-list">
+                    {colorInfo.combination.map((combinationColor, i) => (
+                      <div
+                        id="recommend-color"
+                        className={colorInfo.combination[i]}
+                        key={i}
+                      >
+                        {combinationColor}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </Container>
     </div>
   );
 };
